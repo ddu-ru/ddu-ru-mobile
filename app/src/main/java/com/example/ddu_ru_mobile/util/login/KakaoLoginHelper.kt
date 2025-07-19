623d52b43b6f599fc698bb19e8a7e80deeecaebf
choi-day <choisschoco@gmail.com>
2025-07-19 23:48:51 +0900

    #1 feat: 버튼 클릭 테스트를 위한 toast메세지 띄우기 구현

--- app/src/main/java/com/example/ddu_ru_mobile/util/login/KakaoLoginHelper.kt
+++ app/src/main/java/com/example/ddu_ru_mobile/util/login/KakaoLoginHelper.kt
@@ -1,7 +1,10 @@
 package com.example.ddu_ru_mobile.util.login
 
-object KakaoLoginHelper {
-    fun kakaoLogin() {
+import android.content.Context
+import android.widget.Toast
 
+object KakaoLoginHelper {
+    fun kakaoLogin(context: Context) {
+        Toast.makeText(context, "카카오로 계속하기 버튼을 클릭", Toast.LENGTH_SHORT).show()
     }
 }
