76091c261d2a7fc0a0a2a4239bcca15a3e74b5c6
choi-day <choisschoco@gmail.com>
2025-07-19 22:27:08 +0900

    #1 feat: 버튼 클릭시 동작하는 LoginActivity.kt 구현

---
+++ app/src/main/java/com/example/ddu_ru_mobile/ui/LoginActivity.kt
@@ -0,0 +1,29 @@
+package com.example.ddu_ru_mobile.ui
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageButton
+import androidx.appcompat.app.AppCompatActivity
+import com.example.ddu_ru_mobile.R
+import com.example.ddu_ru_mobile.util.login.KakaoLoginHelper
+import com.example.ddu_ru_mobile.util.login.GoogleLoginHelper
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)  // XML 연결
+
+        val kakao_btn = findViewById<ImageButton>(R.id.kakao_btn)
+        val google_btn = findViewById<ImageButton>(R.id.google_btn)
+
+        //버튼 클릭시 kakao로그인 실행
+        kakao_btn.setOnClickListener{
+            KakaoLoginHelper.kakaoLogin()
+        }
+
+        //버튼 클릭시 google로그인 실행
+        google_btn.setOnClickListener {
+            GoogleLoginHelper.googleLogin()
+        }
+    }
+}
