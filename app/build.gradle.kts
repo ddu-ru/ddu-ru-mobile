a66c147c3207e5c0607f1aaeaa9840dec97e9dca
choi-day <choisschoco@gmail.com>
2025-08-11 19:44:58 +0900

    #6 chore: 바텀시트를 위한 의존성 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,4 +60,5 @@ dependencies {
 
     implementation("androidx.appcompat:appcompat:1.7.1")
     implementation("com.kizitonwose.calendar:view:2.3.0")
+    implementation("com.google.android.material:material:1.12.0")
 }
