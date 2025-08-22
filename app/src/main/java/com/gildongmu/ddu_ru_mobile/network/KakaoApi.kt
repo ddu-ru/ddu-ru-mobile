b4ae76a517f1ef212611bd2e1826c8fbe0fdb727
choi-day <choisschoco@gmail.com>
2025-08-22 12:34:46 +0900

    #3 fix: url변경

--- app/src/main/java/com/gildongmu/ddu_ru_mobile/network/KakaoApi.kt
+++ app/src/main/java/com/gildongmu/ddu_ru_mobile/network/KakaoApi.kt
@@ -3,11 +3,9 @@ package com.gildongmu.ddu_ru_mobile.network
 import com.gildongmu.ddu_ru_mobile.model.KakaoLoginRequest
 import com.gildongmu.ddu_ru_mobile.model.KakaoLoginResponse
 import retrofit2.http.Body
-import retrofit2.http.Headers
 import retrofit2.http.POST
 
 interface KakaoApi {
-    @POST("/auth/kakao")
-    @Headers("Content-Type:application/json")
+    @POST("auth/kakao")
     suspend fun socialLogin(@Body user: KakaoLoginRequest): KakaoLoginResponse
 }
