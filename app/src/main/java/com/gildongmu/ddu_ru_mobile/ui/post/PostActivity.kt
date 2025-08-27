cee72b02506b4110545dfcda31cd51c06f4d6e7e
choi-day <choisschoco@gmail.com>
2025-08-27 12:58:26 +0900

    #14 fix: toolbar뒤로가기 구현

--- app/src/main/java/com/gildongmu/ddu_ru_mobile/ui/post/PostActivity.kt
+++ app/src/main/java/com/gildongmu/ddu_ru_mobile/ui/post/PostActivity.kt
@@ -5,24 +5,28 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import androidx.fragment.app.Fragment
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import com.gildongmu.ddu_ru_mobile.R
+import com.gildongmu.ddu_ru_mobile.databinding.ActivityPostBinding
 
 class PostActivity : AppCompatActivity() {
 
     private lateinit var navController: NavController
+    private lateinit var postBinding: ActivityPostBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_post)
+        postBinding = ActivityPostBinding.inflate(layoutInflater)
+        setContentView(postBinding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.postLayoutMain)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        setupToolbar()
 
         // Navigation Controller 초기화
         val navHostFragment = supportFragmentManager
@@ -30,15 +34,16 @@ class PostActivity : AppCompatActivity() {
         navController = navHostFragment.navController
     }
 
-    // Fragment 전환을 위한 헬퍼 메서드 (Navigation 사용 시에는 불필요하지만 호환성을 위해 유지)
-    fun replaceFragment(fragment: Fragment, addToBackStack: Boolean = true) {
-        val transaction = supportFragmentManager.beginTransaction()
-            .replace(R.id.postLayoutMain, fragment)
-
-        if (addToBackStack) {
-            transaction.addToBackStack(null)
+    private fun setupToolbar() {
+        setSupportActionBar(postBinding.toolbar)
+        supportActionBar?.apply {
+            setDisplayHomeAsUpEnabled(true)  // 뒤로가기 버튼 표시
+            setDisplayShowTitleEnabled(false) // 타이틀 완전히 숨기기
         }
 
-        transaction.commit()
+        // 뒤로가기 버튼 클릭 시 finish()
+        postBinding.toolbar.setNavigationOnClickListener {
+            onBackPressed()
+        }
     }
 }
