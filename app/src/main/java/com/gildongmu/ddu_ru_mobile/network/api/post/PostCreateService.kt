0ff4d5da0c16a7425226074b53039800d7184283
choi-day <choisschoco@gmail.com>
2025-08-31 00:38:44 +0900

    #14 fix: API요청 헤더 추가

--- app/src/main/java/com/gildongmu/ddu_ru_mobile/network/api/post/PostCreateService.kt
+++ app/src/main/java/com/gildongmu/ddu_ru_mobile/network/api/post/PostCreateService.kt
@@ -4,8 +4,11 @@ import com.gildongmu.ddu_ru_mobile.model.post.request.PostCreateRequest
 import com.gildongmu.ddu_ru_mobile.model.post.response.PostCreateResponse
 import retrofit2.http.POST
 import retrofit2.http.Body
+import retrofit2.http.Header
 
 interface PostCreateService {
     @POST("posts")
-    suspend fun postCreate(@Body post: PostCreateRequest): PostCreateResponse
+    suspend fun postCreate(
+        @Header("Authorization") authorization: String,
+        @Body post: PostCreateRequest): PostCreateResponse
 }
